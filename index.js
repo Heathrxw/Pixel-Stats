5ffa0fb433691f8618800e67e926e807d4b6fd44
Heathrxw <Heathrxw@gmail.com>
2022-02-04 12:03:12 +0000

    Created connection instance

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//Import modules
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
-
+const config = require('./config.json')
+const { createConnection } = require('mysql')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+//Read Commands
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -12,21 +16,38 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
+//Bot Online
+
 client.once('ready', () => {
 	console.log('Pixel Stats is now online!');
+	client.user.setActivity(`Hypixel Stats Bot, /help`);
 });
 client.on('interactionCreate', interaction => {
     console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction!`);
-    client.user.setActivity(`Hypixel Stats Bot, /help`);
+});
+
+//Connect to MySQL database for user stats logging
+
+let con = createConnection(config.mysql);
+con.connect(err => {
+    if(err)return console.log('Connection to the database has been lost!' + err);
+    console.log("Connecting to database...");
+    console.log("Connected to database!");
 })
+
+//Notifier for guild join
 
 client.on("guildCreate", guild => {
 	console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`)
 })
 
+//Notifer for guild leave
+
 client.on("guildDelete", guild => {
-	console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+	console.log(`Removed from guild: ${guild.name} (id: ${guild.id})`);
   });
+
+//Creates slash commands
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
@@ -42,5 +63,7 @@ client.on('interactionCreate', async interaction => {
 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
+
+//Logs in with bot token
 
 client.login(token);
