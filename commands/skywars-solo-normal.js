13929b65717e9557d099a8be5662bb489dd2fece
Heathrxw <63247966+Heathrxw@users.noreply.github.com>
2022-02-09 09:10:18 +0000

    Added SQL to skywars normal

--- commands/skywars-solo-normal.js
+++ commands/skywars-solo-normal.js
@@ -3,6 +3,10 @@ const { MessageEmbed } = require('discord.js');
 const {color, footer } = require('../config.json')
 const hypixelAPIReborn = require('../hypixel.js')
 const commaNumber = require('comma-number');
+const config = require('../config.json')
+const { createConnection } = require('mysql2');
+let con = createConnection(config.mysql);
+const fetch = require('node-fetch');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,20 +16,29 @@ module.exports = {
 
     async execute(interaction) {;
         const username = interaction.options.getString('username');
+        const uuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+        const playerUUIDData = await uuid.json();
         hypixelAPIReborn.getPlayer(username).then((player) => {
+            const Kills = (player.stats.skywars.solo.normal.kills)
+            const Deaths = (player.stats.skywars.solo.normal.deaths)
+            const Wins = (player.stats.skywars.solo.normal.wins)
+            const Losses = (player.stats.skywars.solo.normal.losses)
+            const KDR = (player.stats.skywars.solo.normal.KDRatio)
+            const WLR = (player.stats.skywars.solo.normal.WLRatio)
             const soloNormal = new MessageEmbed()
             .setColor(color)
             .setTitle(`${player}'s Solo Normal Skywars Statistics`)
             .setThumbnail('https://hypixel.net/styles/hypixel-v2/images/game-icons/Skywars-64.png')
-            .addField('Kills', commaNumber(player.stats.skywars.solo.normal.kills), true)
-            .addField('Deaths', commaNumber(player.stats.skywars.solo.normal.deaths), true)
-            .addField('Wins', commaNumber(player.stats.skywars.solo.normal.wins), true)
-            .addField('Losses', commaNumber(player.stats.skywars.solo.normal.losses), true)
-            .addField('WLR', commaNumber(player.stats.skywars.solo.normal.WLRatio), true)
-            .addField('KDR', commaNumber(player.stats.skywars.solo.normal.KDRatio), true)
+            .addField('Kills', commaNumber(Kills), true)
+            .addField('Deaths', commaNumber(Deaths), true)
+            .addField('Wins', commaNumber(Wins), true)
+            .addField('Losses', commaNumber(Losses), true)
+            .addField('WLR', commaNumber(WLR), true)
+            .addField('KDR', commaNumber(WLR), true)
             .setTimestamp()
-            .setFooter({text: footer });
+            .setFooter({text: footer, iconURL: `https://visage.surgeplay.com/face/256/${playerUUIDData.id}.png` });
             interaction.reply({ embeds: [soloNormal] });
+            con.query(`INSERT INTO Skywars (Mode,Username,Kills,Deaths,Wins,Losses,KDR,WLR) VALUES ('Solo','${username}','${Kills}','${Deaths}','${Wins}','${Losses}','${KDR}','${WLR}')`)
         }).catch((err) =>{
             interaction.reply(`${username} is not a valid name! Are they nicked?`);
             console.log(err)
