c36f52e09f1d318ca99c609f7bc3471e9f0c7084
Heathrxw <63247966+Heathrxw@users.noreply.github.com>
2022-02-09 09:19:00 +0000

    Added SQL to ranked

--- commands/skywars-ranked.js
+++ commands/skywars-ranked.js
@@ -3,6 +3,10 @@ const hypixelAPIReborn = require('../hypixel.js')
 const { MessageEmbed } = require('discord.js');
 const {color, footer } = require('../config.json')
 const commaNumber = require('comma-number');
+const config = require('../config.json')
+const { createConnection } = require('mysql2');
+let con = createConnection(config.mysql);
+const fetch = require('node-fetch');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,21 +16,31 @@ module.exports = {
 
     async execute(interaction) {;
         const username = interaction.options.getString('username');
+        const uuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+        const playerUUIDData = await uuid.json();
         hypixelAPIReborn.getPlayer(username).then((player) => {
+            const Kills = (player.stats.skywars.solo.ranked.kills)
+            const Deaths = (player.stats.skywars.solo.ranked.deaths)
+            const Wins = (player.stats.skywars.solo.ranked.wins)
+            const Losses = (player.stats.skywars.solo.ranked.losses)
+            const KDR = (player.stats.skywars.solo.ranked.KDRatio)
+            const WLR = (player.stats.skywars.solo.ranked.WLRatio)
+            const playedGames = (player.stats.skywars.solo.ranked.playedGames)
             const ranked = new MessageEmbed()
             .setColor(color)
             .setTitle(`${player}'s Ranked Skywars Statistics`)
             .setThumbnail('https://hypixel.net/styles/hypixel-v2/images/game-icons/Skywars-64.png')
-            .addField('Kills', commaNumber(player.stats.skywars.ranked.kills), true)
-            .addField('Deaths', commaNumber(player.stats.skywars.ranked.deaths), true)
-            .addField('Wins', commaNumber(player.stats.skywars.ranked.wins), true)
-            .addField('Losses', commaNumber(player.stats.skywars.ranked.losses), true)
-            .addField('KDR', commaNumber(player.stats.skywars.ranked.KDRatio), true)
-            .addField('WLR', commaNumber(player.stats.skywars.ranked.WLRatio), true)
-            .addField('Played Games', commaNumber(player.stats.skywars.ranked.playedGames), true)
+            .addField('Kills', commaNumber(Kills), true)
+            .addField('Deaths', commaNumber(Deaths), true)
+            .addField('Wins', commaNumber(Wins), true)
+            .addField('Losses', commaNumber(Losses), true)
+            .addField('KDR', commaNumber(KDR), true)
+            .addField('WLR', commaNumber(WLR), true)
+            .addField('Played Games', commaNumber(playedGames), true)
             .setTimestamp()
-            .setFooter({ text: footer });
+            .setFooter({ text: footer, iconURL: `https://visage.surgeplay.com/face/256/${playerUUIDData.id}.png`  });
             interaction.reply({ embeds: [ranked] });
+            con.query(`INSERT INTO Skywars (Mode,Username,Kills,Deaths,Wins,Losses,KDR,WLR,playedGames) VALUES ('Ranked','${username}','${Kills}','${Deaths}','${Wins}','${Losses}','${KDR}','${WLR}','${playedGames}')`)
         }).catch((err) => {
             interaction.reply(`${username} is not a valid name! Are they nicked?`)
             console.log(err)
