cae8e18c86d6f15c2538e122f1a179976100b25b
Heathrxw <Heathrxw@gmail.com>
2022-02-10 23:59:42 +0000

    added blitz

--- commands/duels-blitz.js
+++ commands/duels-blitz.js
@@ -3,12 +3,15 @@ const hypixelAPIReborn = require('../hypixel.js')
 const { MessageEmbed } = require('discord.js');
 const {color, footer } = require('../config.json')
 const commaNumber = require('comma-number');
+const config = require('../config.json')
+const { createConnection } = require('mysql2');
+let con = createConnection(config.mysql);
 const fetch = require('node-fetch');
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('duels-blitz')
-    .setDescription('Gets specified players stats for blitz duels statistics')
+    .setDescription('Gets specified players stats for Blitz duels statistics')
     .addStringOption(option => option.setName('username').setRequired(true).setDescription('The username of the player you want to get the statistics of')),
 
     async execute(interaction) {;
@@ -16,22 +19,32 @@ module.exports = {
         const uuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
         const playerUUIDData = await uuid.json();
         hypixelAPIReborn.getPlayer(username).then((player) => {
+            const Kills = (player.stats.duels.blitz.kills)
+            const Deaths = (player.stats.duels.blitz.deaths)
+            const Wins = (player.stats.duels.blitz.wins)
+            const Losses = (player.stats.duels.blitz.losses)
+            const KDR = (player.stats.duels.blitz.KDRatio)
+            const WLR = (player.stats.duels.blitz.WLRatio)
+            const playedGames = (player.stats.duels.blitz.playedGames)
+            const Winstreak = (player.stats.duels.blitz.winstreak)
+            const bestWinstreak = (player.stats.duels.blitz.bestWinstreak)
             const blitz = new MessageEmbed()
             .setColor(color)
             .setTitle(`${player}'s Blitz Duels Statistics`)
             .setThumbnail('https://hypixel.net/styles/hypixel-v2/images/game-icons/Duels-64.png')
-            .addField('Kills', commaNumber(player.stats.duels.blitz.kills), true)
-            .addField('Deaths', commaNumber(player.stats.duels.blitz.deaths), true)
-            .addField('Wins', commaNumber(player.stats.duels.blitz.wins), true)
-            .addField('Losses', commaNumber(player.stats.duels.blitz.losses), true)
-            .addField('KDR', commaNumber(player.stats.duels.blitz.KDRatio), true)
-            .addField('WLR', commaNumber(player.stats.duels.blitz.WLRatio), true)
-            .addField('Played Games', commaNumber(player.stats.duels.blitz.playedGames), true)
-            .addField('Winstreak', commaNumber(player.stats.duels.blitz.winstreak), true)
-            .addField('Best Winstreak', commaNumber(player.stats.duels.blitz.bestWinstreak), true)
+            .addField('Kills', commaNumber(Kills), true)
+            .addField('Deaths', commaNumber(Deaths), true)
+            .addField('Wins', commaNumber(Wins), true)
+            .addField('Losses', commaNumber(Losses), true)
+            .addField('KDR', commaNumber(KDR), true)
+            .addField('WLR', commaNumber(WLR), true)
+            .addField('Played Games', commaNumber(playedGames), true)
+            .addField('Winstreak', commaNumber(Winstreak), true)
+            .addField('Best Winstreak', commaNumber(bestWinstreak), true)
             .setTimestamp()
             .setFooter({ text: footer, iconURL: `https://visage.surgeplay.com/face/256/${playerUUIDData.id}.png`  });
             interaction.reply({ embeds: [blitz] });
+            con.query(`INSERT INTO Duels (Mode,Username,Kills,Deaths,Wins,Losses,KDR,WLR,playedGames,Winstreak,bestWinstreak) VALUES ('Blitz','${username}','${Kills}','${Deaths}','${Wins}','${Losses}','${KDR}','${WLR}','${playedGames}','${Winstreak}','${bestWinstreak}')`)
         }).catch((err) => {
             interaction.reply(`${username} is not a valid name! Are they nicked?`)
             console.log(err)
