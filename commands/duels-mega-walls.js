f794567400a2b48c2d6f3645cf739eb56810ee52
Heathrxw <Heathrxw@gmail.com>
2022-02-12 18:11:20 +0000

    added mega walls

---
+++ commands/duels-mega-walls.js
@@ -0,0 +1,55 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const hypixelAPIReborn = require('../hypixel.js')
+const { MessageEmbed } = require('discord.js');
+const {color, footer } = require('../config.json')
+const commaNumber = require('comma-number');
+const config = require('../config.json')
+const { createConnection } = require('mysql2');
+let con = createConnection(config.mysql);
+const fetch = require('node-fetch');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('duels-mega-walls')
+    .setDescription('Gets specified players stats for Mega Walls duels statistics')
+    .addStringOption(option => option.setName('username').setRequired(true).setDescription('The username of the player you want to get the statistics of')),
+
+    async execute(interaction) {;
+        const username = interaction.options.getString('username');
+        const uuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+        const playerUUIDData = await uuid.json();
+        hypixelAPIReborn.getPlayer(username).then((player) => {
+            const Kills = (player.stats.duels.megawalls.kills)
+            const Deaths = (player.stats.duels.megawalls.deaths)
+            const Wins = (player.stats.duels.megawalls.wins)
+            const Losses = (player.stats.duels.megawalls.losses)
+            const KDR = (player.stats.duels.megawalls.KDRatio)
+            const WLR = (player.stats.duels.megawalls.WLRatio)
+            const playedGames = (player.stats.duels.megawalls.playedGames)
+            const Winstreak = (player.stats.duels.megawalls.winstreak)
+            const bestWinstreak = (player.stats.duels.megawalls.bestWinstreak)
+            const Division = (player.stats.duels.megawalls.division)
+            const megawalls = new MessageEmbed()
+            .setColor(color)
+            .setTitle(`${player}'s Mega Walls Duels Statistics`)
+            .setThumbnail('https://hypixel.net/styles/hypixel-v2/images/game-icons/Duels-64.png')
+            .addField('Kills', commaNumber(Kills), true)
+            .addField('Deaths', commaNumber(Deaths), true)
+            .addField('Wins', commaNumber(Wins), true)
+            .addField('Losses', commaNumber(Losses), true)
+            .addField('KDR', commaNumber(KDR), true)
+            .addField('WLR', commaNumber(WLR), true)
+            .addField('Played Games', commaNumber(playedGames), true)
+            .addField('Winstreak', commaNumber(Winstreak), true)
+            .addField('Best Winstreak', commaNumber(bestWinstreak), true)
+            .addField('Division', commaNumber(Division), true)
+            .setTimestamp()
+            .setFooter({ text: footer, iconURL: `https://visage.surgeplay.com/face/256/${playerUUIDData.id}.png`  });
+            interaction.reply({ embeds: [megawalls] });
+            con.query(`INSERT INTO Duels (Mode,Username,Kills,Deaths,Wins,Losses,KDR,WLR,playedGames,Winstreak,bestWinstreak,Division) VALUES ('MegaWalls','${username}','${Kills}','${Deaths}','${Wins}','${Losses}','${KDR}','${WLR}','${playedGames}','${Winstreak}','${bestWinstreak}','${Division}')`)
+        }).catch((err) => {
+            interaction.reply(`${username} is not a valid name! Are they nicked? If not do /help`)
+            console.log(err)
+        })
+    }
+}
