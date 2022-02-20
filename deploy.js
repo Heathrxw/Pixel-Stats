b3a81ca5462dcae3f39c2a176b2291d859ce7ef7
Heathrxw <Heathrxw@gmail.com>
2022-02-20 19:36:06 +0000

    Added sub folders finally lol

--- deploy.js
+++ deploy.js
@@ -4,10 +4,40 @@ const { Routes } = require('discord-api-types/v9');
 const { clientID, guildID, token } = require('./config.json');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFilesCore = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFilesSkywars = fs.readdirSync('./commands/Skywars').filter(file => file.endsWith('.js'));
+const commandFilesBedwars = fs.readdirSync('./commands/Bedwars').filter(file => file.endsWith('.js'));
+const commandFilesDuels = fs.readdirSync('./commands/Duels').filter(file => file.endsWith('.js'));
+const commandFilesClassic = fs.readdirSync('./commands/Classic').filter(file => file.endsWith('.js'));
+const commandFilesExternal = fs.readdirSync('./commands/External').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
+for (var file of commandFilesCore) {
 	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+for (var file of commandFilesSkywars) {
+	const command = require(`./commands/Skywars/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+for (var file of commandFilesBedwars) {
+	const command = require(`./commands/Bedwars/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+for (var file of commandFilesDuels) {
+	const command = require(`./commands/Duels/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+for (var file of commandFilesClassic) {
+	const command = require(`./commands/Classic/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+for (var file of commandFilesExternal) {
+	const command = require(`./commands/External/${file}`);
 	commands.push(command.data.toJSON());
 }
 
