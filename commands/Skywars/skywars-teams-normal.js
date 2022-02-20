567c7bc370ffe972e771d70c081335e328408a20
Heathrxw <Heathrxw@gmail.com>
2022-02-20 19:35:38 +0000

    Added skywars to own folder

---
+++ commands/Skywars/skywars-teams-normal.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const hypixelAPIReborn = require('../../hypixel.js')
+const { MessageEmbed } = require('discord.js');
+const {color, footer } = require('../../config.json')
+const commaNumber = require('comma-number');
+const config = require('../../config.json')
+const { createConnection } = require('mysql2');
+let con = createConnection(config.mysql);
+const fetch = require('node-fetch');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('skywars-teams-normal')
+    .setDescription('Gets specified players stats for teams normal skywars statistics')
+    .addStringOption(option => option.setName('username').setRequired(true).setDescription('The username of the player you want to get the statistics of')),
+
+    async execute(interaction) {;
+        const username = interaction.options.getString('username');
+        const uuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+        const playerUUIDData = await uuid.json();
+        hypixelAPIReborn.getPlayer(username).then((player) => {
+            const Kills = (player.stats.skywars.team.normal.kills)
+            const Deaths = (player.stats.skywars.team.normal.deaths)
+            const Wins = (player.stats.skywars.team.normal.wins)
+            const Losses = (player.stats.skywars.team.normal.losses)
+            const KDR = (player.stats.skywars.team.normal.KDRatio)
+            const WLR = (player.stats.skywars.team.normal.WLRatio)
+            const teamsnormal = new MessageEmbed()
+            .setColor(color)
+            .setTitle(`${player}'s Team Skywars Normal Statistics`)
+            .setThumbnail('https://hypixel.net/styles/hypixel-v2/images/game-icons/Skywars-64.png')
+            .addField('Kills', commaNumber(Kills), true)
+            .addField('Deaths', commaNumber(Deaths), true)
+            .addField('Wins', commaNumber(Wins), true)
+            .addField('Losses', commaNumber(Losses), true)
+            .addField('KDR', commaNumber(KDR), true)
+            .addField('WLR', commaNumber(WLR), true)
+            .setTimestamp()
+            .setFooter({ text: footer, iconURL: `https://visage.surgeplay.com/face/256/${playerUUIDData.id}.png`});
+            interaction.reply({ embeds: [teamsnormal] });
+            con.query(`INSERT INTO Skywars (Mode,Username,Kills,Deaths,Wins,Losses,KDR,WLR) VALUES ('TeamsNormal','${username}','${Kills}','${Deaths}','${Wins}','${Losses}','${KDR}','${WLR}')`)
+        }).catch((err) => {
+            interaction.reply(`${username} is not a valid name! Are they nicked?`)
+            console.log(err)
+        });
+    }
+}
---
+++ commands/Skywars/skywars.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const {color, footer } = require('../../config.json')
+const hypixelAPIReborn = require('../../hypixel.js')
+const commaNumber = require('comma-number');
+const config = require('../../config.json')
+const { createConnection } = require('mysql2');
+let con = createConnection(config.mysql);
+const fetch = require('node-fetch');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('skywars')
+    .setDescription('Gets specified players Skywars statistics')
+    .addStringOption(option => option.setName('username').setRequired(true).setDescription('The username of the player you want to get the statistics of')),
+
+    async execute(interaction) {;
+        const username = interaction.options.getString('username');
+        const uuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+        const playerUUIDData = await uuid.json();
+        hypixelAPIReborn.getPlayer(username).then((player) => {
+            const Kills = (player.stats.skywars.kills)
+            const Deaths = (player.stats.skywars.deaths)
+            const Wins = (player.stats.skywars.wins)
+            const Losses = (player.stats.skywars.losses)
+            const KDR = (player.stats.skywars.KDRatio)
+            const WLR = (player.stats.skywars.WLRatio)
+            const Coins = (player.stats.skywars.coins)
+            const Tokens = (player.stats.skywars.tokens)
+            const Level = (player.stats.skywars.level)
+            const Heads = (player.stats.skywars.heads)
+            const Prestige = (player.stats.skywars.prestige)
+            const Souls = (player.stats.skywars.souls)
+            const skywarsEmbed = new MessageEmbed()
+             .setColor(color)
+             .setTitle(`${player}'s Skywars Overall Statistics`)
+             .setThumbnail('https://hypixel.net/styles/hypixel-v2/images/game-icons/Skywars-64.png')
+             .addField('Kills', commaNumber(Kills), true)
+             .addField('Deaths', commaNumber(Deaths), true)
+             .addField('Wins', commaNumber(Wins), true)
+             .addField('Losses', commaNumber(Losses), true)
+             .addField('KDR', commaNumber(KDR), true)
+             .addField('WLR', commaNumber(WLR), true)
+             .addField('Total Coins', commaNumber(Coins), true)
+             .addField('Total Tokens', commaNumber(Tokens), true)
+             .addField('Level', commaNumber(Level), true)
+             .addField('Heads', commaNumber(Heads), true)
+             .addField('Prestige', commaNumber(Prestige), true)
+             .addField('Souls', commaNumber(Souls), true)
+             .setTimestamp()
+             .setFooter({ text: footer, iconURL: `https://visage.surgeplay.com/face/256/${playerUUIDData.id}.png`   });
+            interaction.reply({ embeds: [skywarsEmbed] });
+            con.query(`INSERT INTO Skywars (Mode,Username,Kills,Deaths,Wins,Losses,KDR,WLR,Coins,Tokens,Level,Heads,Prestige,Souls) VALUES ('Overall','${username}','${Kills}','${Deaths}','${Wins}','${Losses}','${KDR}','${WLR}','${Coins}','${Tokens}','${Level}','${Heads}','${Prestige}','${Souls}')`)
+        }).catch((err) => {
+            interaction.reply(`"${username}" is not a valid name! Are they nicked?`);
+            console.log(err);
+        });
+    }
+}
