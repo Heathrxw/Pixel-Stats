ab787acf862cdc67b8fd5976d040256b065d223d
Heathrxw <Heathrxw@gmail.com>
2022-02-21 22:07:29 +0000

    Added blitz

--- deploy.js
+++ deploy.js
@@ -4,15 +4,16 @@ const { Routes } = require('discord-api-types/v9');
 const { clientID, guildID, token } = require('./config.json');
 
 const commands = [];
-const commandFilesCore = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFilesCore = fs.readdirSync('./commands/Core').filter(file => file.endsWith('.js'));
 const commandFilesSkywars = fs.readdirSync('./commands/Skywars').filter(file => file.endsWith('.js'));
 const commandFilesBedwars = fs.readdirSync('./commands/Bedwars').filter(file => file.endsWith('.js'));
 const commandFilesDuels = fs.readdirSync('./commands/Duels').filter(file => file.endsWith('.js'));
 const commandFilesClassic = fs.readdirSync('./commands/Classic').filter(file => file.endsWith('.js'));
 const commandFilesExternal = fs.readdirSync('./commands/External').filter(file => file.endsWith('.js'));
+const commandFilesBlitz = fs.readdirSync('./commands/Blitz').filter(file => file.endsWith('.js'));
 
 for (var file of commandFilesCore) {
-	const command = require(`./commands/${file}`);
+	const command = require(`./commands/Core/${file}`);
 	commands.push(command.data.toJSON());
 }
 
@@ -38,6 +39,11 @@ for (var file of commandFilesClassic) {
 
 for (var file of commandFilesExternal) {
 	const command = require(`./commands/External/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+for (var file of commandFilesBlitz) {
+	const command = require(`./commands/Blitz/${file}`);
 	commands.push(command.data.toJSON());
 }
 
