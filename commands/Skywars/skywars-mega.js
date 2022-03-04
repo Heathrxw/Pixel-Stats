d065429b428c7a3fb216708ff1311159aefa4197
Heathrxw <Heathrxw@gmail.com>
2022-03-04 15:43:16 +0000

    formatted skywars mega

--- commands/Skywars/skywars-mega.js
+++ commands/Skywars/skywars-mega.js
@@ -1,49 +1,68 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const hypixelAPIReborn = require('../../hypixel.js')
-const { MessageEmbed } = require('discord.js');
-const {color, footer } = require('../../config.json')
-const commaNumber = require('comma-number');
-const config = require('../../config.json')
-const { createConnection } = require('mysql2');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const hypixelAPIReborn = require("../../hypixel.js");
+const { MessageEmbed } = require("discord.js");
+const { color, footer } = require("../../config.json");
+const commaNumber = require("comma-number");
+const config = require("../../config.json");
+const { createConnection } = require("mysql2");
 let con = createConnection(config.mysql);
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('skywars-mega')
-    .setDescription('Gets specified players stats for mega skywars statistics')
-    .addStringOption(option => option.setName('username').setRequired(true).setDescription('The username of the player you want to get the statistics of')),
+  data: new SlashCommandBuilder()
+    .setName("skywars-mega")
+    .setDescription("Gets specified players stats for mega skywars statistics")
+    .addStringOption((option) =>
+      option
+        .setName("username")
+        .setRequired(true)
+        .setDescription(
+          "The username of the player you want to get the statistics of"
+        )
+    ),
 
-    async execute(interaction) {;
-        const username = interaction.options.getString('username');
-        const uuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
-        const playerUUIDData = await uuid.json();
-        hypixelAPIReborn.getPlayer(username).then((player) => {
-            const Kills = (player.stats.skywars.solo.overall.kills)
-            const Deaths = (player.stats.skywars.solo.overall.deaths)
-            const Wins = (player.stats.skywars.solo.overall.wins)
-            const Losses = (player.stats.skywars.solo.overall.losses)
-            const KDR = (player.stats.skywars.solo.overall.KDRatio)
-            const WLR = (player.stats.skywars.solo.overall.WLRatio)
-            const playedGames = (player.stats.skywars.solo.overall.playedGames)
-            const mega = new MessageEmbed()
-            .setColor(color)
-            .setTitle(`${player}'s Mega Skywars Statistics`)
-            .setThumbnail('https://hypixel.net/styles/hypixel-v2/images/game-icons/Skywars-64.png')
-            .addField('Kills', commaNumber(Kills), true)
-            .addField('Deaths', commaNumber(Deaths), true)
-            .addField('Wins', commaNumber(Wins), true)
-            .addField('Losses', commaNumber(Losses), true)
-            .addField('KDR', commaNumber(KDR), true)
-            .addField('WLR', commaNumber(WLR), true)
-            .addField('Games Played', commaNumber(playedGames), true)
-            .setTimestamp()
-            .setFooter({ text: footer, iconURL: `https://visage.surgeplay.com/face/256/${playerUUIDData.id}.png`  });
-            interaction.reply({ embeds: [mega] });
-            con.query(`INSERT INTO Skywars (Mode,Username,Kills,Deaths,Wins,Losses,KDR,WLR,playedGames) VALUES ('Mega','${username}','${Kills}','${Deaths}','${Wins}','${Losses}','${KDR}','${WLR}','${playedGames}')`)
-        }).catch((err) => {
-            interaction.reply(`${username} is not a valid name! Are they nicked?`)
-            console.log(err)
-        });
-    }
-}
+  async execute(interaction) {
+    const username = interaction.options.getString("username");
+    const uuid = await fetch(
+      `https://api.mojang.com/users/profiles/minecraft/${username}`
+    );
+    const playerUUIDData = await uuid.json();
+    hypixelAPIReborn
+      .getPlayer(username)
+      .then((player) => {
+        const Kills = player.stats.skywars.solo.overall.kills;
+        const Deaths = player.stats.skywars.solo.overall.deaths;
+        const Wins = player.stats.skywars.solo.overall.wins;
+        const Losses = player.stats.skywars.solo.overall.losses;
+        const KDR = player.stats.skywars.solo.overall.KDRatio;
+        const WLR = player.stats.skywars.solo.overall.WLRatio;
+        const playedGames = player.stats.skywars.solo.overall.playedGames;
+        const mega = new MessageEmbed()
+          .setColor(color)
+          .setTitle(`${player}'s Mega Skywars Statistics`)
+          .setThumbnail(
+            "https://hypixel.net/styles/hypixel-v2/images/game-icons/Skywars-64.png"
+          )
+          .addField("Kills", commaNumber(Kills), true)
+          .addField("Deaths", commaNumber(Deaths), true)
+          .addField("Wins", commaNumber(Wins), true)
+          .addField("Losses", commaNumber(Losses), true)
+          .addField("KDR", commaNumber(KDR), true)
+          .addField("WLR", commaNumber(WLR), true)
+          .addField("Games Played", commaNumber(playedGames), true)
+          .setTimestamp()
+          .setFooter({
+            text: footer,
+            iconURL: `https://visage.surgeplay.com/face/256/${playerUUIDData.id}.png`,
+          });
+        interaction.reply({ embeds: [mega] });
+        con.query(
+          `INSERT INTO Skywars (Mode,Username,Kills,Deaths,Wins,Losses,KDR,WLR,playedGames) VALUES ('Mega','${username}','${Kills}','${Deaths}','${Wins}','${Losses}','${KDR}','${WLR}','${playedGames}')`
+        );
+      })
+      .catch((err) => {
+        interaction.reply(`${username} is not a valid name! Are they nicked?`);
+        console.log(err);
+      });
+  },
+};
