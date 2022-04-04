6619ceb9018bc17e95323f6854236914d63b2b4a
Heathrxw <Heathrxw@gmail.com>
2022-04-04 23:57:21 +0100

    added red vs diamond

---
+++ commands/UHC/uhc-redVSblue.js
@@ -0,0 +1,66 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const { color, footer } = require("../../config.json");
+const hypixelAPIReborn = require("../../hypixel.js");
+const commaNumber = require("comma-number");
+const config = require("../../config.json");
+const { createConnection } = require("mysql2");
+let con = createConnection(config.mysql);
+const fetch = require("node-fetch");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("uhc-redvsblue")
+    .setDescription("Gets specified players UHC Red Vs Blue statistics")
+    .addStringOption((option) =>
+      option
+        .setName("username")
+        .setRequired(true)
+        .setDescription(
+          "The username of the player you want to get the statistics of"
+        )
+    ),
+
+  async execute(interaction) {
+    const username = interaction.options.getString("username");
+    const uuid = await fetch(
+      `https://api.mojang.com/users/profiles/minecraft/${username}`
+    );
+    const playerUUIDData = await uuid.json();
+    hypixelAPIReborn
+      .getPlayer(username)
+      .then((player) => {
+        const Kills = player.stats.uhc.redVsBlue.kills;
+        const Deaths = player.stats.uhc.redVsBlue.deaths;
+        const Wins = player.stats.uhc.redVsBlue.wins;
+        const KDR = (Kills / Deaths).toFixed(2);
+        const headsEaten = player.stats.uhc.redVsBlue.headsEaten;
+        const UHCRVB = new MessageEmbed()
+          .setColor(color)
+          .setTitle(`${player}'s UHC Red Vs Blue Statistics`)
+          .setThumbnail(
+            "https://hypixel.net/styles/hypixel-v2/images/game-icons/UHC-64.png"
+          )
+          .addField("Kills", commaNumber(Kills), true)
+          .addField("Deaths", commaNumber(Deaths), true)
+          .addField("Wins", commaNumber(Wins), true)
+          .addField("KDR", commaNumber(KDR), true)
+          .addField("Heads Eaten", commaNumber(headsEaten), true)
+          .setTimestamp()
+          .setFooter({
+            text: footer,
+            iconURL: `https://visage.surgeplay.com/face/256/${playerUUIDData.id}.png`,
+          });
+        interaction.reply({ embeds: [UHCRVB] });
+        con.query(
+          `INSERT INTO UHC (Mode,Username,Kills,Deaths,Wins,KDR,HeadsEaten) VALUES ('UHCRVB','${username}','${Kills}','${Deaths}','${Wins}','${KDR}','${headsEaten}')`
+        );
+      })
+      .catch((err) => {
+        interaction.reply(
+          `"${username}" is not a valid name! Are they nicked?`
+        );
+        console.log(err);
+      });
+  },
+};
